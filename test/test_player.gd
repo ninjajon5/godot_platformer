85d066874486fd315d00ab87a586d0b572c9288f
Jon Driver <jon.a.driver.mail@gmail.com>
2026-03-15 21:24:07 +0000

    added state tests for all states

--- test/test_player.gd
+++ test/test_player.gd
@@ -11,13 +11,6 @@ func after_each() -> void:
 	player.free()
 	
 
-func test_jump_from_resting_decreases_y_velocity() -> void:
-	player.state = Player.State.RESTING
-	player._jump()
-	
-	assert_true(player.velocity.y < 0)
-
-
 func test_airborne_with_upwards_velocity_causes_jumping_state() -> void:
 	player.on_floor = false
 	player.velocity = Vector2(0, -100)
@@ -32,3 +25,26 @@ func test_airborne_with_downwards_velocity_causes_falling_state() -> void:
 	player._update_state()
 	
 	assert_eq(player.state, Player.State.FALLING)
+
+
+func test_grounded_with_no_velocity_causes_resting_state() -> void:
+	player.on_floor = true
+	player.velocity = Vector2(0, 0)
+	player._update_state()
+	
+	assert_eq(player.state, Player.State.RESTING)
+
+
+func test_grounded_with_non_zero_velocity_causes_walking_state() -> void:
+	player.on_floor = true
+	player.velocity = Vector2(100, 0)
+	player._update_state()
+	
+	assert_eq(player.state, Player.State.WALKING)
+
+
+func test_jump_from_resting_decreases_y_velocity() -> void:
+	player.state = Player.State.RESTING
+	player._jump()
+	
+	assert_true(player.velocity.y < 0)
