9754bb398e10e00b91e06c3f46396c978a787858
Jon Driver <jon.a.driver.mail@gmail.com>
2026-09-14 22:04:16 +0100

    game working with cs conversion

--- debug.cs
+++ debug.cs
@@ -4,25 +4,25 @@ namespace Game;
 
 public partial class Debug : Label
 {
-    private Player _player;
-    private float _maxInputAxis = -1.0f;
-    private string _state;
-    private string[] _recentStates = { "-", "-", "-", "-", "-" };
+	private Player _player;
+	private float _maxInputAxis = -1.0f;
+	private string _state;
+	private string[] _recentStates = { "-", "-", "-", "-", "-" };
 
-    public override void _Ready()
-    {
-        _player = GetNode<Player>("../../Player");
-    }
+	public override void _Ready()
+	{
+		_player = GetNode<Player>("../../Player");
+	}
 
-    public override void _Process(double delta)
-    {
-        if ( _player.inputs.axis > _maxInputAxis ) _maxInputAxis = _player.inputs.axis;
+	public override void _Process(double delta)
+	{
+		if ( _player.inputs.axis > _maxInputAxis ) _maxInputAxis = _player.inputs.axis;
 
-        _state = _player.state.ToString();
-        UpdateRecentStates(_state);
+		_state = _player.state.ToString();
+		UpdateRecentStates(_state);
 
-        Text = $@"state: {_state}
-        recentStates: [ {_recentStates[0]}, {_recentStates[1]}, {_recentStates[2]}, {_recentStates[3]}, {_recentStates[4]}, {_recentStates[5]} ]
+		Text = $@"state: {_state}
+        recentStates: [ {_recentStates[0]}, {_recentStates[1]}, {_recentStates[2]}, {_recentStates[3]}, {_recentStates[4]} ]
         dashingFrameCount: {_player.dashingFrameCount}
         jumpsquatFrameCount: {_player.jumpsquatFrameCount}
         smashingStick: {_player.inputs.smashingStick}
@@ -31,19 +31,19 @@ public partial class Debug : Label
         inputDirection: {_player.inputs.direction}
         inputAxis: {_player.inputs.axis}
         maxInputDirection: {_maxInputAxis}
-        speed: {_player.Velocity.X}";
-    }
+		speed: {_player.Velocity.X}";
+	}
 
-    private void UpdateRecentStates(string newState)
-    {
-        int endIndex = System.Array.IndexOf(_recentStates, newState);
-        if ( endIndex == -1 ) endIndex = _recentStates.Length - 1;
+	private void UpdateRecentStates(string newState)
+	{
+		int endIndex = System.Array.IndexOf(_recentStates, newState);
+		if ( endIndex == -1 ) endIndex = _recentStates.Length - 1;
 
-        for ( int i = endIndex; i > 0; i-- )
-        {
-            _recentStates[i] = _recentStates[i - 1];
-        }
+		for ( int i = endIndex; i > 0; i-- )
+		{
+			_recentStates[i] = _recentStates[i - 1];
+		}
 
-        _recentStates[0] = newState;
-    }
+		_recentStates[0] = newState;
+	}
 }
--- player.cs
+++ player.cs
@@ -6,8 +6,8 @@ namespace Game;
 
 /*
  * TODO
- * convert files to cs
- * get tests passing
+ * set up xUnit demo test
+ * get tests passing using
  * refactor animation
  * pause menu
  * debug in pause menu
@@ -15,411 +15,411 @@ namespace Game;
 
 public partial class Player: CharacterBody2D {
 
-    // constants
-    private const float _WalkingSpeed = 400.0f;
-    private const float _RunningSpeed = 600.0f;
-    private const float _Friction = 25.0f;
-    private const float _Acceleration = 50.0f;
-    private const float _Gravity = 30.0f;
-    private const float _JumpVelocity = -600.0f;
-    private const float _BackflipVelocity = 250.0f;
-    private const int _FastFallingMultiplier = 4;
-    private const int _DashingFrames = 15;
-    private const int _JumpsquatFrames = 4;
-
-    // inputs
-    public InputReader inputs = new();
-
-    // state tracking
-    public enum State
-    {
-        Resting,
-        Walking,
-        Running,
-        RunTurnaround,
-        Dashing,
-        DashRelease,
-        Jumpsquat,
-        Jumping,
-        Falling,
-        FastFalling
-    }
-    private readonly State[] _GroundedInactionableStates = [ State.RunTurnaround, State.Jumpsquat ];
-    public State state;
-    public int dashingFrameCount = 0;
-    public int jumpsquatFrameCount = 0;
-
-    // physics attributes
-    public bool onFloor;
-    public bool fastFalling = false;
-    public Vector2 gravityVector;
-
-    public override void _PhysicsProcess(double delta)
-    {
-        inputs.ReadInputs();
-        ReadPhysics();
-
-        CheckForPhysicsTransitions();
-        ApplyInputsDependingOnState();
-
-        MoveAndSlide();
-    }
-
-
-    private void ReadPhysics()
-    {
-        onFloor = IsOnFloor();
-        gravityVector = new Vector2(0, _Gravity);
-    }
-
-
-    private void CheckForPhysicsTransitions()
-    {
-        if( !onFloor && Velocity.Y >= 0 && state != State.FastFalling )
-        {
-            state = State.Falling;
-        }
-        else if ( onFloor && Velocity.X == 0 && state != State.Jumpsquat )
-        {
-            state = State.Resting;
-        }
-        else if ( onFloor && ( state == State.Falling | state == State.FastFalling ) )
-        {
-            state = State.Walking;
-        }
-    }
-
-
-    private void ApplyInputsDependingOnState()
-    {
-        ResetInputsIfInactionable();
-        switch(state)
-        {
-            case State.Resting: 
-                ApplyInputsToRestingState();
-                break;
-            case State.Walking:
-                ApplyInputsToWalkingState();
-                break;
-            case State.Running:
-                ApplyInputsToRunningState();
-                break;
-            case State.RunTurnaround:
-                ApplyInputsToRunTurnaroundState();
-                break;
-            case State.Dashing:
-                ApplyInputsToDashingState();
-                break;
-            case State.DashRelease:
-                ApplyInputsToDashReleaseState();
-                break;
-            case State.Jumpsquat:
-                ApplyInputsToJumpsquatState();
-                break;
-            case State.Jumping:
-                ApplyInputsToJumpingState();
-                break;
-            case State.Falling:
-                ApplyInputsToFallingState();
-                break;
-            case State.FastFalling:
-                ApplyInputsToFastFallingState();
-                break;
-        }
-    }
-
-
-
-    private void ResetInputsIfInactionable()
-    {
-        if ( _GroundedInactionableStates.Contains(state) && inputs.smashingStick ) inputs.ResetSmashingStick();
-    }
-
-
-    private void ApplyInputsToRestingState()
-    {
-        if ( inputs.direction != 0 )
-        {
-            if ( inputs.smashingStick )
-            {
-                Dash();
-            }
-            else
-            {
-                Walk();
-            }
-        }
-        else
-        {
-            Rest();
-        }
-
-        if ( inputs.jump ) Jumpsquat();
-    }
-
-
-    private void ApplyInputsToWalkingState()
-    {
-        if ( inputs.smashingStick )
-        {
-            Dash();
-        }
-        else
-        {
-            Walk();
-        }
-
-        if ( inputs.jump ) Jumpsquat();
-    }
-
-
-    private void ApplyInputsToRunningState()
-    {
-        if ( inputs.direction != 0 )
-        {
-            if ( InputOpposesDirection() )
-            {
-                RunTurnaround();
-            }
-            else
-            {
-                Run();
-            }
-        }
-        else
-        {
-            ApplyFriction();
-        }
-
-        if ( inputs.jump ) Jumpsquat();
-    }
-
-
-    private void ApplyInputsToRunTurnaroundState()
-    {
-        RunTurnaround();
-        if ( inputs.jump ) Jumpsquat();
-    }
-
-
-    private void ApplyInputsToDashingState()
-    {
-        if ( inputs.direction != 0 && inputs.smashingStick )
-        {
-            if ( dashingFrameCount <= _DashingFrames )
-            {
-                Dash();
-            }
-            else
-            {
-                Run();
-            }
-        }
-        else
-        {
-            DashRelease();
-            ApplyFriction();
-        }
-
-        if ( inputs.jump ) Jumpsquat();
-    }
-
-
-    private void ApplyInputsToDashReleaseState()
-    {
-        if ( inputs.direction != 0 && InputOpposesDirection() && inputs.smashingStick )
-        {
-            Dash();
-        }
-        else
-        {
-            ApplyFriction();
-        }
-
-        if ( inputs.jump ) Jumpsquat();
-    }
-
-
-    private void ApplyInputsToJumpsquatState()
-    {
-        if ( jumpsquatFrameCount >= _JumpsquatFrames )
-        {
-            Jump();
-        }
-        else
-        {
-            Jumpsquat();
-        }
-    }
-
-
-    private void ApplyInputsToJumpingState()
-    {
-        ApplyGravity(1);
-    }
-
-
-    private void ApplyInputsToFallingState()
-    {
-        if( inputs.crouch )
-        {
-            state = State.FastFalling;
-            ApplyGravity(_FastFallingMultiplier);
-        }
-        else
-        {
-            ApplyGravity(1);
-        }
-
-        if ( GetNode<AnimatedSprite2D>("AnimatedSprite2D").Animation != "backflip" )
-        {
-            GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("jumping");
-        }
-    }
-
-
-    private void ApplyInputsToFastFallingState()
-    {
-        ApplyGravity(_FastFallingMultiplier);
-    }
-
-
-    private void ApplyGravity(int gravityMultiplier)
-    {
-        Vector2 velocity = Velocity;
-        velocity += gravityVector * gravityMultiplier;
-        Velocity = velocity;
-
-        if ( GetNode<AnimatedSprite2D>("AnimatedSprite2D").Animation != "backflip" )
-        {
-            GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("jumping");
-        }
-    }
-
-
-
-    private void ApplyFriction()
-    {
-        Velocity = new Vector2
-        (
-            Mathf.MoveToward( Velocity.X, 0, _Friction ),
-            Velocity.Y
-        );
-    }
-
-
-    private void Rest()
-    {
-        dashingFrameCount = 0;
-        GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("resting");
-    }
-
-
-    private void Walk()
-    {
-        state = State.Walking;
-        Velocity = new Vector2
-        (
-            Mathf.MoveToward( Velocity.X, _WalkingSpeed * inputs.axis, _Acceleration ),
-            Velocity.Y
-        );
-        GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("walking");
-        FlipAnimationBasedOnDirection();
-    }
-
-
-
-    private void Run()
-    {
-        state = State.Running;
-        GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("walking");
-        FlipAnimationBasedOnDirection();
-    }
-
-
-    private void RunTurnaround()
-    {
-        if ( state != State.RunTurnaround )
-        {
-            state = State.RunTurnaround;
-            inputs.smashingStick = false;
-            FlipAnimationBasedOnDirection();
-        }
-        ApplyFriction();
-    }
-
-
-    private void Dash()
-    {
-        state = State.Dashing;
-        
-        if ( InputOpposesDirection() ) dashingFrameCount = 0;
-
-        Velocity = new Vector2( ( inputs.direction * _RunningSpeed ), Velocity.Y );
-        dashingFrameCount += 1;
-
-        GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("resting");
-        FlipAnimationBasedOnDirection();
-    }
-
-
-    private void DashRelease()
-    {
-        state = State.DashRelease;
-        dashingFrameCount = 0;
-    }
-
-
-    private void Jumpsquat()
-    {
-        state = State.Jumpsquat;
-        jumpsquatFrameCount += 1;
-        GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("jumpsquat");
-    }
-
-
-    private void Jump()
-    {
-        state = State.Jumping;
-        fastFalling = false;
-        jumpsquatFrameCount = 0;
-        Velocity = new Vector2(Velocity.X, _JumpVelocity);
-        if ( InputOpposesFacing() )
-        {
-            Velocity = new Vector2( (_BackflipVelocity * inputs.direction), Velocity.Y );
-            GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("backflip");
-        } 
-        else if ( GetNode<AnimatedSprite2D>("AnimatedSprite2D").Animation != "backflip" )
-        {
-            GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("jumping");
-        }
-    }
-
-
-    private void FlipAnimationBasedOnDirection()
-    {
-        GetNode<AnimatedSprite2D>("AnimatedSprite2D").FlipH = inputs.direction < 0 ;
-    }
-
-    
-    private bool InputOpposesDirection()
-    {
-        return
-        (
-            Math.Sign(inputs.direction) != Math.Sign(Velocity.X)
-            &&
-            inputs.direction != 0
-        );
-    }
-
-
-    private bool InputOpposesFacing()
-    {
-        return
-        (
-            ( inputs.direction > 0 && GetNode<AnimatedSprite2D>("AnimatedSprite2D").FlipH == true )
-            || 
-            ( inputs.direction < 0 && GetNode<AnimatedSprite2D>("AnimatedSprite2D").FlipH == false )
-        );
-    }
+	// constants
+	private const float _WalkingSpeed = 400.0f;
+	private const float _RunningSpeed = 600.0f;
+	private const float _Friction = 25.0f;
+	private const float _Acceleration = 50.0f;
+	private const float _Gravity = 30.0f;
+	private const float _JumpVelocity = -600.0f;
+	private const float _BackflipVelocity = 250.0f;
+	private const int _FastFallingMultiplier = 4;
+	private const int _DashingFrames = 15;
+	private const int _JumpsquatFrames = 4;
+
+	// inputs
+	public InputReader inputs = new();
+
+	// state tracking
+	public enum State
+	{
+		Resting,
+		Walking,
+		Running,
+		RunTurnaround,
+		Dashing,
+		DashRelease,
+		Jumpsquat,
+		Jumping,
+		Falling,
+		FastFalling
+	}
+	private readonly State[] _GroundedInactionableStates = [ State.RunTurnaround, State.Jumpsquat ];
+	public State state;
+	public int dashingFrameCount = 0;
+	public int jumpsquatFrameCount = 0;
+
+	// physics attributes
+	public bool onFloor;
+	public bool fastFalling = false;
+	public Vector2 gravityVector;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		inputs.ReadInputs();
+		ReadPhysics();
+
+		CheckForPhysicsTransitions();
+		ApplyInputsDependingOnState();
+
+		MoveAndSlide();
+	}
+
+
+	private void ReadPhysics()
+	{
+		onFloor = IsOnFloor();
+		gravityVector = new Vector2(0, _Gravity);
+	}
+
+
+	private void CheckForPhysicsTransitions()
+	{
+		if( !onFloor && Velocity.Y >= 0 && state != State.FastFalling )
+		{
+			state = State.Falling;
+		}
+		else if ( onFloor && Velocity.X == 0 && state != State.Jumpsquat )
+		{
+			state = State.Resting;
+		}
+		else if ( onFloor && ( state == State.Falling | state == State.FastFalling ) )
+		{
+			state = State.Walking;
+		}
+	}
+
+
+	private void ApplyInputsDependingOnState()
+	{
+		ResetInputsIfInactionable();
+		switch(state)
+		{
+			case State.Resting: 
+				ApplyInputsToRestingState();
+				break;
+			case State.Walking:
+				ApplyInputsToWalkingState();
+				break;
+			case State.Running:
+				ApplyInputsToRunningState();
+				break;
+			case State.RunTurnaround:
+				ApplyInputsToRunTurnaroundState();
+				break;
+			case State.Dashing:
+				ApplyInputsToDashingState();
+				break;
+			case State.DashRelease:
+				ApplyInputsToDashReleaseState();
+				break;
+			case State.Jumpsquat:
+				ApplyInputsToJumpsquatState();
+				break;
+			case State.Jumping:
+				ApplyInputsToJumpingState();
+				break;
+			case State.Falling:
+				ApplyInputsToFallingState();
+				break;
+			case State.FastFalling:
+				ApplyInputsToFastFallingState();
+				break;
+		}
+	}
+
+
+
+	private void ResetInputsIfInactionable()
+	{
+		if ( _GroundedInactionableStates.Contains(state) && inputs.smashingStick ) inputs.ResetSmashingStick();
+	}
+
+
+	private void ApplyInputsToRestingState()
+	{
+		if ( inputs.direction != 0 )
+		{
+			if ( inputs.smashingStick )
+			{
+				Dash();
+			}
+			else
+			{
+				Walk();
+			}
+		}
+		else
+		{
+			Rest();
+		}
+
+		if ( inputs.jump ) Jumpsquat();
+	}
+
+
+	private void ApplyInputsToWalkingState()
+	{
+		if ( inputs.smashingStick )
+		{
+			Dash();
+		}
+		else
+		{
+			Walk();
+		}
+
+		if ( inputs.jump ) Jumpsquat();
+	}
+
+
+	private void ApplyInputsToRunningState()
+	{
+		if ( inputs.direction != 0 )
+		{
+			if ( InputOpposesDirection() )
+			{
+				RunTurnaround();
+			}
+			else
+			{
+				Run();
+			}
+		}
+		else
+		{
+			ApplyFriction();
+		}
+
+		if ( inputs.jump ) Jumpsquat();
+	}
+
+
+	private void ApplyInputsToRunTurnaroundState()
+	{
+		RunTurnaround();
+		if ( inputs.jump ) Jumpsquat();
+	}
+
+
+	private void ApplyInputsToDashingState()
+	{
+		if ( inputs.direction != 0 && inputs.smashingStick )
+		{
+			if ( dashingFrameCount <= _DashingFrames )
+			{
+				Dash();
+			}
+			else
+			{
+				Run();
+			}
+		}
+		else
+		{
+			DashRelease();
+			ApplyFriction();
+		}
+
+		if ( inputs.jump ) Jumpsquat();
+	}
+
+
+	private void ApplyInputsToDashReleaseState()
+	{
+		if ( inputs.direction != 0 && InputOpposesDirection() && inputs.smashingStick )
+		{
+			Dash();
+		}
+		else
+		{
+			ApplyFriction();
+		}
+
+		if ( inputs.jump ) Jumpsquat();
+	}
+
+
+	private void ApplyInputsToJumpsquatState()
+	{
+		if ( jumpsquatFrameCount >= _JumpsquatFrames )
+		{
+			Jump();
+		}
+		else
+		{
+			Jumpsquat();
+		}
+	}
+
+
+	private void ApplyInputsToJumpingState()
+	{
+		ApplyGravity(1);
+	}
+
+
+	private void ApplyInputsToFallingState()
+	{
+		if( inputs.crouch )
+		{
+			state = State.FastFalling;
+			ApplyGravity(_FastFallingMultiplier);
+		}
+		else
+		{
+			ApplyGravity(1);
+		}
+
+		if ( GetNode<AnimatedSprite2D>("AnimatedSprite2D").Animation != "backflip" )
+		{
+			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("jumping");
+		}
+	}
+
+
+	private void ApplyInputsToFastFallingState()
+	{
+		ApplyGravity(_FastFallingMultiplier);
+	}
+
+
+	private void ApplyGravity(int gravityMultiplier)
+	{
+		Vector2 velocity = Velocity;
+		velocity += gravityVector * gravityMultiplier;
+		Velocity = velocity;
+
+		if ( GetNode<AnimatedSprite2D>("AnimatedSprite2D").Animation != "backflip" )
+		{
+			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("jumping");
+		}
+	}
+
+
+
+	private void ApplyFriction()
+	{
+		Velocity = new Vector2
+		(
+			Mathf.MoveToward( Velocity.X, 0, _Friction ),
+			Velocity.Y
+		);
+	}
+
+
+	private void Rest()
+	{
+		dashingFrameCount = 0;
+		GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("resting");
+	}
+
+
+	private void Walk()
+	{
+		state = State.Walking;
+		Velocity = new Vector2
+		(
+			Mathf.MoveToward( Velocity.X, _WalkingSpeed * inputs.axis, _Acceleration ),
+			Velocity.Y
+		);
+		GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("walking");
+		FlipAnimationBasedOnDirection();
+	}
+
+
+
+	private void Run()
+	{
+		state = State.Running;
+		GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("walking");
+		FlipAnimationBasedOnDirection();
+	}
+
+
+	private void RunTurnaround()
+	{
+		if ( state != State.RunTurnaround )
+		{
+			state = State.RunTurnaround;
+			inputs.smashingStick = false;
+			FlipAnimationBasedOnDirection();
+		}
+		ApplyFriction();
+	}
+
+
+	private void Dash()
+	{
+		state = State.Dashing;
+		
+		if ( InputOpposesDirection() ) dashingFrameCount = 0;
+
+		Velocity = new Vector2( ( inputs.direction * _RunningSpeed ), Velocity.Y );
+		dashingFrameCount += 1;
+
+		GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("resting");
+		FlipAnimationBasedOnDirection();
+	}
+
+
+	private void DashRelease()
+	{
+		state = State.DashRelease;
+		dashingFrameCount = 0;
+	}
+
+
+	private void Jumpsquat()
+	{
+		state = State.Jumpsquat;
+		jumpsquatFrameCount += 1;
+		GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("jumpsquat");
+	}
+
+
+	private void Jump()
+	{
+		state = State.Jumping;
+		fastFalling = false;
+		jumpsquatFrameCount = 0;
+		Velocity = new Vector2(Velocity.X, _JumpVelocity);
+		if ( InputOpposesFacing() )
+		{
+			Velocity = new Vector2( (_BackflipVelocity * inputs.direction), Velocity.Y );
+			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("backflip");
+		} 
+		else if ( GetNode<AnimatedSprite2D>("AnimatedSprite2D").Animation != "backflip" )
+		{
+			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("jumping");
+		}
+	}
+
+
+	private void FlipAnimationBasedOnDirection()
+	{
+		GetNode<AnimatedSprite2D>("AnimatedSprite2D").FlipH = inputs.direction < 0 ;
+	}
+
+	
+	private bool InputOpposesDirection()
+	{
+		return
+		(
+			Math.Sign(inputs.direction) != Math.Sign(Velocity.X)
+			&&
+			inputs.direction != 0
+		);
+	}
+
+
+	private bool InputOpposesFacing()
+	{
+		return
+		(
+			( inputs.direction > 0 && GetNode<AnimatedSprite2D>("AnimatedSprite2D").FlipH == true )
+			|| 
+			( inputs.direction < 0 && GetNode<AnimatedSprite2D>("AnimatedSprite2D").FlipH == false )
+		);
+	}
 }
